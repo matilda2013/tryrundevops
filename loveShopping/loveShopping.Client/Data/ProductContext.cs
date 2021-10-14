96ec83ef9df2b3e138e85d9e6ec6e3bd033a4463
hola.holu101@gmail.com <hola.holu101@gmail.com>
2021-10-14 10:24:30 +0100

    added new Product

--- loveShopping/loveShopping.Client/Data/ProductContext.cs
+++ loveShopping/loveShopping.Client/Data/ProductContext.cs
@@ -58,7 +58,16 @@ namespace loveShopping.Client.Data
                     ImageFile = "product-6.png",
                     Price = 240.00M,
                     Category = "Home Kitchen"
+                },
+                new Product()
+                {
+                    Name = "LG G7 ThinQ EndofCourse",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-6.png",
+                    Price = 240.00M,
+                    Category = "Home Zion"
                 }
+
         };
 }
 }
